38386cab247c2d1bfa0ce19f89638da2ee548d86
Serena Dominguez <serenadominguez@gmail.com>
2019-06-17 15:04:36 -0400

    30% done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,52 +1,56 @@
-function deleteItem(e){
+window.onload = function() {
+  // EXAMINE THE DOCUMENT OBJECT
 
-}
+  // console.dir(document);
+  // console.log(document.domain);
+  // console.log(document.URL);
+  // console.log(document.title);
+  // document.title = TEST;
+  // console.log(document.doctype);
+  // console.log(document.head);
+  // console.log(document.body);
+  // console.log(document.all);
 
-function getPriceByProduct(itemNode){
-
-}
-
-function updatePriceByProduct(productPrice, index){
-
-}
-
-function getTotalPrice() {
-
-}
-
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
-
-window.onload = function(){
-  var calculatePriceButton = document.getElementById('calc-prices-button');
-  var createItemButton = document.getElementById('new-item-create');
-  var deleteButtons = document.getElementsByClassName('btn-delete');
+  var calculatePriceButton = document.getElementById("calc-prices-button");
+  var createItemButton = document.getElementById("new-item-create");
+  var deleteButtons = document.getElementsByClassName("btn-delete");
+  //   var itemPrice = document.getElementById("price");
+  //  price.style.fontWeight = "bold";
 
   calculatePriceButton.onclick = getTotalPrice;
   createItemButton.onclick = createNewItem;
 
-  for(var i = 0; i<deleteButtons.length ; i++){
+  for (var i = 0; i < deleteButtons.length; i++) {
     deleteButtons[i].onclick = deleteItem;
+
+    function deleteItem(e) {}
+
+    function getPriceByProduct(itemNode) {}
+
+    function updatePriceByProduct(productPrice, index) {}
+
+    // Create a click event for the Calculate Prices button
+    // This event will execute a function that:
+    // Retrieves the unit price of the product
+    // Retrieves the quantity of items desired
+    // Calculates the total price based on this data
+    // Updates the total price in the DOM
+    function getTotalPrice() {
+      // .maps?
+      itemUnitPrice, quantityOfItems;
+      // itemUnitPrice * quantityOfItems = new getTotalPrice
+    }
+
+    function createQuantityInput() {}
+
+    function createDeleteButton() {}
+
+    function createQuantityNode() {}
+
+    function createItemNode(dataType, itemData) {}
+
+    function createNewItemRow(itemName, itemUnitPrice) {}
+
+    function createNewItem() {}
   }
 };
